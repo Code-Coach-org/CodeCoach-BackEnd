e17d286cee0275102fceb6709411a2771f0689fc
youngmin <min050410@gmail.com>
2022-10-04 14:02:36 +0900

    add: User mvc 패턴 정의, 엔티티 정의, 환경변수 설정, DB연결

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PW,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      logging: true,
+      entities: [__dirname + '/**/entities/*.entity.{js,ts}']
+    }),],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
